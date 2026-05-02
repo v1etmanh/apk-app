ee1176db5599f724b60470e70364bc57a6f4187e
LE Viet Manh <mqnyle@gmail.com>
2026-05-02 23:04:33 +0700

    ad

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { supabase } from '../store/suppabase'; // 👈 import supabase client
 
-const API_BASE_URL = 'http://192.168.1.19:5001';
+const API_BASE_URL = 'https://dailyserver-production.up.railway.app/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
